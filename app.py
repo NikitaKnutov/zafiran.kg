8b132b67cf97a3b47eb694da109866dabbc1238d
NikitaKnutov <nikita.cnutov228@gmail.com>
2025-10-14 13:27:32 +0300

    Complete project rewrite

--- app.py
+++ app.py
@@ -14,33 +14,18 @@ def services():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/software')
-def software():
-    return render_template('software.html')
+# @app.route('/licenses')
+# def software():
+#     return render_template('licenses.html')
 
 @app.route('/advantages')
 def advantages():
     return render_template('advantages.html')
 
-@app.route('/aml')
-def aml():
-    return render_template('aml.html')
-
-@app.route('/team')
-def team():
-    return render_template('team.html')
-
 @app.route('/documents')
 def documents():
     return render_template('documents.html')
 
-@app.route('/products')
-def products():
-    return render_template('products.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
